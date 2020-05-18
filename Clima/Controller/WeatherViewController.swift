df9e49a998eb19db3524e6d811402e38d2864e1e
Sushi-UX <46356596+Sushi-UX@users.noreply.github.com>
2020-05-18 17:30:04 -0400

    Initial commit

---
+++ Clima/Controller/WeatherViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  NotClima
+//
+//  Created by Raymond Choy on 5/18/20.
+//  Copyright © 2020 thechoygroup. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    var weatherManager = WeatherManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        searchTextField.delegate = self
+    }
+
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+        print(searchTextField.text!)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+            searchTextField.endEditing(true)
+            print(searchTextField.text!)
+            return true
+        }
+        
+        func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+            if textField.text != "" {
+                return true
+            } else {
+                textField.placeholder = "Type something"
+                return false
+            }
+        }
+        
+        func textFieldDidEndEditing(_ textField: UITextField) {
+            
+            if let city = searchTextField.text {
+                weatherManager.fetchWeather(cityName: city)
+            }
+            
+            searchTextField.text = ""
+        }
+        
+
+        
+    }
+
